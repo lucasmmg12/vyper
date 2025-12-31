e3abae68eb5d74c74d274185f52143e40539c249
lucasmmg12 <lucas@growsanjuan.com>
2025-12-31 13:01:44 -0300

    style: set logogrow.png as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Vyper Labs",
   description: "Retail & Loyalty Platform",
+  icons: {
+    icon: "/logogrow.png",
+  },
 };
 
 export default function RootLayout({
